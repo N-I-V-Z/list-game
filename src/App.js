bca5cb90b4d1e3086d580a087b2e2681982ddd4a
N-I-V-Z <vothanhnhan106@gmail.com>
2024-07-27 17:34:27 +0700

    delete link of bird

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import CaroGame from "./component/CaroGame3x3/CaroGame";
 import ListGame from "./component/List/ListGame";
-import Birds from "./component/flapbird/Bird";
 import CaroGame5 from "./component/CaroGame5/CaroGame";
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<ListGame />} />
           <Route path="/caro-game-3x3" element={<CaroGame />} />
-          <Route path="/trym" element={<Birds />} />
           <Route path="/caro-game-5" element={<CaroGame5 />} />
         </Routes>
       </div>
