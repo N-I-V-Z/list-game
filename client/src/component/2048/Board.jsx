b58e5c124eeeed2821da7d5c57fe5a565a4f20f4
Thinhlp176 <phuocthinh17623@gmail.com>
2024-07-28 19:38:00 +0700

    fix UI

--- client/src/component/2048/Board.jsx
+++ client/src/component/2048/Board.jsx
@@ -3,13 +3,13 @@ import './Board.css';
 
 const Board = ({ board }) => {
     return (
-        <div className="board">
+        <div className="board-2048">
             {board.map((row, rowIndex) => (
-                <div key={rowIndex} className="board-row">
+                <div key={rowIndex} className="board-row-2048">
                     {row.map((cell, cellIndex) => (
                         <div
                             key={cellIndex}
-                            className={`board-cell ${cell !== 0 ? 'filled' : ''}`}
+                            className={`board-cell-2048 ${cell !== 0 ? 'filled' : ''}`}
                             data-value={cell}
                         >
                             {cell !== 0 ? cell : ''}
